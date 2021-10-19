c092879f349d1fb2edb4175cd4025203f3538911
Ivan <kalmych@gmail.com>
2021-10-19 10:12:53 +0300

    changes after reviewing

--- src/pro/sky/lesson5/Main.java
+++ src/pro/sky/lesson5/Main.java
@@ -19,26 +19,25 @@ public class Main {
         // month - October
         int friday = 1; //
         for (friday = 1; friday <= 31; friday++) {
-            if (friday % 7 == 1)
-                System.out.println("Сегодня пятница " + friday + ". Необходимо подготовить отчёт!");
+            if (friday % 7 == 1){
+                System.out.println("Сегодня пятница " + friday + ". Необходимо подготовить отчёт!");}
         }
         System.out.println();
 
         System.out.println("Задание №3");
         int currentYear = 2021;
         int p = currentYear - 200;
-        for (p = 1821; p <= 2121; p++) {
-            if (p % 79 == 0)
-                System.out.println(p);
+        for (p = (currentYear-200); p <= (currentYear+100); p++) {
+            if (p % 79 == 0){
+                System.out.println(p);}
         }
         System.out.println();
 
         System.out.println("Задание 4");
         int d = 0;
-        while (d <= 29) {
-            d++;
-            if (d % 5 == 0 && d % 3 == 0)
-                System.out.println(d + " ping pong");
+        for (d=1; d < 31; d++) {
+        if (d % 5 == 0 && d % 3 == 0){
+                System.out.println(d + " ping pong");}
             else if (d % 3 == 0)
                 System.out.println(d + " ping");
             else if (d % 5 == 0)
